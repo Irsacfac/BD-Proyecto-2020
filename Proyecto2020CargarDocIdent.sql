db19303a3fca4a15cf7140c157e075c806f71675
Irsacfac <irsacfac@gmail.com>
2020-11-11 21:37:01 -0600

    Update Proyecto2020CargarDocIdent.sql
    Se modifico y se agregaron comentarios

--- Proyecto2020CargarDocIdent.sql
+++ Proyecto2020CargarDocIdent.sql
@@ -1,8 +1,8 @@
 USE [Proyecto];
 GO
-DROP Procedure  IF EXISTS Cargar_Tipo_Doc;
+DROP Procedure  IF EXISTS dbo.CargarTipoDoc;
 GO
-CREATE PROCEDURE Cargar_Tipo_Doc
+CREATE PROCEDURE dbo.CargarTipoDoc
 AS
 BEGIN
 SET NOCOUNT ON
@@ -12,9 +12,9 @@ SET NOCOUNT ON
 			@doc INT =0
 		SELECT @tipo_Documento = od	FROM OPENROWSET (BULK 'D:\S3\Proyecto2020\Datos_Tarea1 v2.xml', SINGLE_BLOB) AS TiposDoc(od) --pone la direccion donde se encuentra
 		EXEC sp_xml_preparedocument @doc OUTPUT, @tipo_Documento
-		INSERT INTO [dbo].[TipoDocumentoIdentidad](Id, Nombre) --selecciona la tabla a llenar y las columnas
-		SELECT * FROM OPENXML (@doc, 'Datos/Tipo_Doc/TipoDocuIdentidad' ,2)   --encabezado a buscar y cantidad de elementos a agregar 2
-		WITH(Id [INT]'@Id',Nombre [VARCHAR](100)'@Nombre');
+		INSERT INTO [dbo].[TipoDocumentoIdentidad](Id, Nombre)				--selecciona la tabla a llenar y las columnas
+		SELECT * FROM OPENXML (@doc, 'Datos/Tipo_Doc/TipoDocuIdentidad')	--encabezado a buscar
+		WITH(Id [INT]'@Id',Nombre [VARCHAR](100)'@Nombre');					--nombre de los datos de la tabla
 	END TRY
 
 	BEGIN CATCH
@@ -23,5 +23,5 @@ SET NOCOUNT ON
 SET NOCOUNT OFF
 END
 GO
-EXEC Cargar_Tipo_Doc
-select * from [dbo].[TipoDocumentoIdentidad]
+EXEC dbo.CargarTipoDoc
+SELECT * FROM [dbo].[TipoDocumentoIdentidad]
