3d2b2f5acfba41c5500404bcf3ec2d7dbe86512d
Irsacfac <irsacfac@gmail.com>
2020-11-11 21:29:47 -0600

    Update Proyecto2020CargarXML.sql
    Se agregaron comentarios a Proyecto2020CargarXML

--- Proyecto2020CargarXML.sql
+++ Proyecto2020CargarXML.sql
@@ -1,13 +1,15 @@
 USE [Proyecto]
 GO
-CREATE PROCEDURE dbo.CargarXML
+DROP Procedure  IF EXISTS dbo.CargarXML;
+GO
+CREATE PROCEDURE dbo.CargarXML													--startprocedure para descargar el xml
 AS
 BEGIN
 SET NOCOUNT ON
-	EXEC msdb.dbo.rds_download_from_s3
-			@s3_arn_of_file='arn:aws:s3:::proyecto2020/Datos_Tarea1 v2.xml',
-			@rds_file_path='D:\S3\Proyecto2020\Datos_Tarea1 v2.xml',
-			@overwrite_file=1;
+	EXEC msdb.dbo.rds_download_from_s3											--Para descargr el documento del S#
+			@s3_arn_of_file='arn:aws:s3:::proyecto2020/Datos_Tarea1 v2.xml',	--Documento a descargar
+			@rds_file_path='D:\S3\Proyecto2020\Datos_Tarea1 v2.xml',			--Donde almacenar el documento
+			@overwrite_file=1;													--Sobreescribir el archivo
 SET NOCOUNT OFF
 END
 GO
